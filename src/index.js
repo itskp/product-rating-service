370ecf434cc68c98075fa3eeea3cf7ab94a15724
Kapil Gajarmal <kapil.gajarmal@theredpandas.com>
2020-01-12 23:17:12 +0530

    chores: add error handling for server start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const app = require('./app')
 
-app.init()
+try {
+    console.log('Starting Server')
+    app.init()
+} catch (err) {
+    console.log('Server start failed')
+    console.log(JSON.stringify(err))
+    process.exit(1)
+}
